421487a2a553a5afdb9c9f8d07ea4c2a0ba47394
rajeshpalei <165571599+rajeshpalei@users.noreply.github.com>
2025-04-27 18:43:20 +0530

    Update tomcat.groovy

--- vars/tomcat.groovy
+++ vars/tomcat.groovy
@@ -1,5 +1,5 @@
-def call (user,ip,credID) {
-sshagent(['${credID}']) {
+def call (user,ip) {
+sshagent(['tomcat-11-ec2']) {
    sh 'scp -o StrictHostKeyChecking=no target/hr-api.war ${user}@${ip}:/opt/tomcat11/webapps/'
    sh 'ssh ${user}@${ip} /opt/tomcat11/bin/shutdown.sh'
    sh 'ssh ${user}@${ip} sudo /opt/tomcat11/bin/startup.sh'
